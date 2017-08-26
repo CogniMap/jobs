ce626e255e6e0b17b6ab2d272f4acc597afa4d4f
Barnabe Chauvin <barnabe.chauvin@gmail.com>
2017-08-27 00:02:01 +0200

    Fix.

---
+++ src/common.ts
@@ -0,0 +1,7 @@
+export namespace ExecutionErrorType {
+  export const CANNOT_FIND_TASK = 'CANNOT_FIND_TASK'; 
+  export const CANNOT_START_TASK = 'CANNOT_START_TASK';  // Could not start the excution of the task
+  export const SCHEDULER_ERROR = 'SCHEDULER_ERROR';    // Could not find what to do for that task configuration
+  export const EXECUTION_FAILED = 'EXECUTION_FAILED';  // The task execution failed
+}
+
